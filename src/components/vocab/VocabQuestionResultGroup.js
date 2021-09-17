ba6ad3d11be55a15bbb91aec9e0be6a92ffe83c5
jdjhaha <jdjhaha1@gmail.com>
2021-09-17 17:54:48 +0900

    학습결과 이력 라인차트 그리기 준비(에빙하우스 망각곡선 뒤에 깔기)

--- src/components/vocab/VocabQuestionResultGroup.js
+++ src/components/vocab/VocabQuestionResultGroup.js
@@ -2,6 +2,32 @@ import React, { useCallback } from 'react';
 import styled, { css } from 'styled-components';
 import palette from '../../lib/styles/palette';
 import { withRouter } from 'react-router-dom';
+import { Line } from 'react-chartjs-2';
+
+const data = {
+  labels: ['1', '2', '3', '4', '5', '6'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      fill: false,
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgba(255, 99, 132, 0.2)',
+    },
+  ],
+};
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
 
 const VocabQuestionResultGroupBlock = styled.div`
   height: calc(100vh - 150px);
@@ -22,17 +48,20 @@ const VocabQuestionResultGroup = ({ vocabQuestionResultGroupList }) => {
     <VocabQuestionResultGroupBlock>
       <div className="container">
         <div className="row">
+          <div className="col-12">
+            <Line data={data} options={options} />
+          </div>
           {vocabQuestionResultGroupList.map((item) => {
             return (
               <div key={item.update_time} className="col-12">
                 <div className="list_box">
-                    <div>{item.group_name}</div>
-                    <div>{item.vocab_count}</div>
-                    <div>{item.answer_count}</div>
-                    <div>{item.wrong_answer_count}</div>
-                    <div>{item.complete_flag}</div>
-                    <div>{item.update_datetime}</div>
-                    <div>{item.study_time_seconds}</div>
+                  <div>{item.group_name}</div>
+                  <div>{item.vocab_count}</div>
+                  <div>{item.answer_count}</div>
+                  <div>{item.wrong_answer_count}</div>
+                  <div>{item.complete_flag}</div>
+                  <div>{item.update_datetime}</div>
+                  <div>{item.study_time_seconds}</div>
                 </div>
               </div>
             );
